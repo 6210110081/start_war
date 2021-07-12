780421841e967f0554267f224c457547f4c29aa2
6210110081 <6210110081@psu.ac.th>
2021-07-12 20:17:54 +0700

    add link

--- lib/starwars_repo.dart
+++ lib/starwars_repo.dart
@@ -1,8 +1,16 @@
+import 'package:dio/dio.dart';
+
 class People {
   String name;
-  People(this.name) {
-    this.name = name;
-  }
+  People(this.name);
 }
 
-class StarwarsRepo {}
+class StarwarsRepo {
+  var dio = Dio();
+  Future<List<People>> repoPeopleStarwar() async {
+    var response = await dio.get('https://swapi.dev/people');
+    print(response);
+    List<People> peopleStarwars = [];
+    return peopleStarwars;
+  }
+}
