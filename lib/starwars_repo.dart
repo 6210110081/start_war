46b510a261466c7d5c22235deb1d074bb5db7d8f
6210110081 <6210110081@psu.ac.th>
2021-07-12 19:47:44 +0700

    make class list & repo

---
+++ lib/starwars_repo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class StarwarsRepo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _StarwarsRepoState();
+  }
+}
+
+class _StarwarsRepoState extends State<StarwarsRepo> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Page Startwars Repo');
+  }
+}
