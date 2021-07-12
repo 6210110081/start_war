d41ebff6b4b4cf4d90154bfd84bf3bf7ea69fbda
6210110081 <6210110081@psu.ac.th>
2021-07-12 20:07:34 +0700

    add people

--- lib/starwars_repo.dart
+++ lib/starwars_repo.dart
@@ -1,15 +1,8 @@
-import 'package:flutter/cupertino.dart';
-
-class StarwarsRepo extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _StarwarsRepoState();
+class People {
+  String name;
+  People(this.name) {
+    this.name = name;
   }
 }
 
-class _StarwarsRepoState extends State<StarwarsRepo> {
-  @override
-  Widget build(BuildContext context) {
-    return Text('Page Startwars Repo');
-  }
-}
+class StarwarsRepo {}
