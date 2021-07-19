e4eb09b5cdf4f5e04a4e092e18112f806bebeb66
6210110081 <6210110081@psu.ac.th>
2021-07-19 18:28:30 +0700

    ui

--- lib/starwars_list.dart
+++ lib/starwars_list.dart
@@ -13,7 +13,6 @@ class _StarwarsListState extends State<StarwarsList> {
   final StarwarsRepo _repo;
   late List<People> _people;
   late int _page;
-  late bool _end;
 
   _StarwarsListState() : _repo = new StarwarsRepo();
 
@@ -23,7 +22,6 @@ class _StarwarsListState extends State<StarwarsList> {
     _page = 1;
     _people = [];
     freshPeople();
-    _end = false;
   }
 
   Future<void> freshPeople() async {
@@ -40,114 +38,124 @@ class _StarwarsListState extends State<StarwarsList> {
     return ListView.builder(
       itemBuilder: (context, index) {
         if (index < _people.length) {
-          return Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Card(
-                  child: Image.network(
-                    'https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg',
-                    height: 200,
-                    width: 200,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Container(
-                        width: 200,
+          return Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Card(
+              margin: EdgeInsets.all(5),
+              shadowColor: Colors.blueGrey,
+              elevation: 10,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Image.network(
+                        'https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg',
                         height: 200,
-                        alignment: Alignment.center,
-                        child: Text('Whoops!', style: TextStyle(fontSize: 30)),
-                      );
-                    },
+                        width: 200,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            width: 200,
+                            height: 200,
+                            alignment: Alignment.center,
+                            child:
+                                Text('Whoops!', style: TextStyle(fontSize: 30)),
+                          );
+                        },
+                      ),
+                    ),
                   ),
-                ),
-                Container(
-                  height: 200,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${_people[index].name}",
-                        style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.red,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            "Height : ",
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.black,
-                                fontWeight: FontWeight.normal),
-                          ),
-                          Text(
-                            "${_people[index].height}",
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.blue,
-                                fontWeight: FontWeight.normal),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            "Gender : ",
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.black,
-                                fontWeight: FontWeight.normal),
-                          ),
-                          Text(
-                            "${_people[index].gender}",
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.blue,
-                                fontWeight: FontWeight.normal),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            "Mass : ",
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.black,
-                                fontWeight: FontWeight.normal),
-                          ),
-                          Text(
-                            "${_people[index].mass}",
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.blue,
-                                fontWeight: FontWeight.normal),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text(
-                            "Birth Year : ",
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.black,
-                                fontWeight: FontWeight.normal),
-                          ),
-                          Text(
-                            "${_people[index].bday}",
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.blue,
-                                fontWeight: FontWeight.normal),
-                          ),
-                        ],
-                      ),
-                    ],
+                  Container(
+                    height: 200,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${_people[index].name}",
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Height : ",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                            Text(
+                              "${_people[index].height}",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Gender : ",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                            Text(
+                              "${_people[index].gender}",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Mass : ",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                            Text(
+                              "${_people[index].mass}",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Birth Year : ",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                            Text(
+                              "${_people[index].bday}",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         } else {
